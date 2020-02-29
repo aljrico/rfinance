246654bcc966dd4825456b5ab3d792ca05e9acce
aljrico <aljrico@gmail.com>
2020-02-29 13:00:54 +0000

    simpler rng

--- R/get_handle.R
+++ R/get_handle.R
@@ -12,7 +12,7 @@ get_handle <- function(){
       random_url <- paste(sample(c(letters, 0:9), 4), collapse = "")
       cu <- paste0("https://finance.yahoo.com?", random_url)
       z <- curl::curl_fetch_memory(cu, handle = h)
-      if (NROW(curl::handle_cookies(h)) > 0){
+      if (nrow(curl::handle_cookies(h)) > 0){
         break
       }
 
@@ -26,7 +26,7 @@ get_handle <- function(){
   }
   
   my_session <- new_session()
-  n <- if (stats::runif(1, 0, 1) >= 0.5) 1L else 2L
+  n <- sample(1:2, 1)
   query_url <- glue::glue("https://query{n}.finance.yahoo.com/v1/test/getcrumb")
   cres <- curl::curl_fetch_memory(query_url, handle = my_session)
   content <- rawToChar(cres$content)
