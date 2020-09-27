7d457493a464ba0d22a639f370c1b43f0927a21d
aljrico <aljrico@gmail.com>
2020-09-27 17:01:13 +0100

    check if data is empty

--- R/ConnectionHandler.R
+++ R/ConnectionHandler.R
@@ -4,80 +4,82 @@
 #' @description This class creates objects to handle connections to the database
 #' @export
 #'
-ConnectionHandler = R6::R6Class(
+ConnectionHandler <- R6::R6Class(
   "ConnectionHandler",
   private = list(
-    log = function(msg){
-      full_msg = paste0("  rfinance: ", msg)
+    log = function(msg) {
+      full_msg <- paste0("  rfinance: ", msg)
       message(full_msg)
     },
     credentials = list(
-      username = NULL,
-      password = NULL
+      username = "consumer-1",
+      password = "HWjZKZcWWkYnU8E8WZaxnFgoAZU8cn"
     ),
-    database = 'secgov-dev',
-    collection = 'visited-links',
-    base_url = 'mongodb+srv://%s:%s@dev-cluster.vvwni.gcp.mongodb.net/%s?retryWrites=true&w=majority',
+    database = "secgov-dev",
+    collection = "visited-links",
+    base_url = "mongodb+srv://%s:%s@dev-cluster.vvwni.gcp.mongodb.net/%s?retryWrites=true&w=majority",
     connection_url = NULL,
     connection_object = NULL,
-    open_connection = function(){
-      private$log("Establishing connection...")
-      private$connection_object = mongolite::mongo(collection = 'visited-links', url = private$connection_url)
+    open_connection = function() {
+      private$connection_object <- mongolite::mongo(collection = "visited-links", url = private$connection_url)
     },
-    close_connection = function(){
+    close_connection = function() {
       private$connection_object$disconnect()
-      private$connection_object = NULL
-      private$log("Connection closed.")
+      private$connection_object <- NULL
     }
   ),
   public = list(
-    initialize = function(username, password){
+    initialize = function(username, password) {
       private$log("Checking credentials...")
-      
-      private$credentials$username = username
-      private$credentials$password = password
-      
-      private$connection_url = sprintf(private$base_url, private$credentials$username, private$credentials$password, private$database)
-      
+
+      private$credentials$username <- username
+      private$credentials$password <- password
+
+      private$connection_url <- sprintf(private$base_url, private$credentials$username, private$credentials$password, private$database)
     },
-    get_statements = function(ticker){
-      download_raw_data <- function(ticker){
-        docs <- mongolite::mongo(collection = 'filings', url = private$connection_url) 
+    get_statements = function(ticker) {
+      download_raw_data <- function(ticker) {
+        docs <- mongolite::mongo(collection = "filings", url = private$connection_url)
         json_string <- paste0('{"TradingSymbol":"', ticker, '"}')
         table_result <- data.table::data.table(docs$find(json_string))
         return(table_result)
       }
-      
-      can_be_numeric <- function(x){
+
+      can_be_numeric <- function(x) {
         is.na(suppressWarnings(as.numeric(x)))
       }
-      format_numerics <- function(dt){
+      format_numerics <- function(dt) {
         data.table::setDT(dt)
-        
-        for(j in 1:ncol(dt)){
-          if(all(can_be_numeric(dt[[j]]))){
+
+        for (j in 1:ncol(dt)) {
+          if (all(can_be_numeric(dt[[j]]))) {
             data.table::set(dt, j = j, value = as.numeric(dt[[j]]))
           }
         }
         return(dt)
       }
-      
+
       # Establish Connection
       private$open_connection()
-      
+
       # Get data
-      private$log("Downloading data...")
-      table_result <- 
-        ticker %>% 
-        download_raw_data() %>% 
-        format_numerics() %>% 
-        janitor::clean_names()
-      
+      private$log(paste0("Downloading data for ", ticker))
+      raw_data <- ticker %>% download_raw_data()
+      if (nrow(raw_data) == 0) {
+        warning(paste0("We can't obtain data from ", ticker))
+        table_result <- NULL
+      } else {
+        table_result <-
+          raw_data %>%
+          format_numerics() %>%
+          janitor::clean_names()
+      }
+
+
       # Close Connection
       private$close_connection()
-      
+
       return(table_result)
-      
     }
   )
 )
