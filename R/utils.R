1acedfa4c1f7cdf73c8af2991fd8e0ee7120eda0
aljrico <aljrico@gmail.com>
2020-12-06 12:15:32 +0100

    add get_sp500

--- R/utils.R
+++ R/utils.R
@@ -12,5 +12,5 @@ NULL
 
 #' Quiets concerns of R CMD check regarding global variables
 if(getRversion() >= "2.15.1"){
-  utils::globalVariables(c(".", "rfinanceConnection"))
+  utils::globalVariables(c(".", "rfinanceConnection", "getTickerFromPage"))
 }
