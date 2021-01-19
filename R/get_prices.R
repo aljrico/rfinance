333ea70c0b0b0e65df9f31979fa56b4534ee37b8
aljrico <aljrico@gmail.com>
2021-01-19 17:14:27 +0000

    herror handling

--- R/get_prices.R
+++ R/get_prices.R
@@ -20,12 +20,19 @@
 #'
 get_prices <- function(symbol, from = '1970-01-01', to = Sys.Date()){
   handle <- get_handle()
-  
   output <- lapply(symbol, function(symbol){
     yahoo_url <- build_yahoo_url(symbol = symbol, from = date_to_unix(from), to = date_to_unix(to), interval = '1d', event = 'history', handle = handle)
-    prices <- readr::read_csv(curl::curl(yahoo_url, handle = handle$session), col_types = readr::cols())
+    prices <- data.table::data.table()
+    
+    tryCatch({
+      prices <- readr::read_csv(curl::curl(yahoo_url, handle = handle$session), col_types = readr::cols())
+      prices$name <- symbol
+    }, 
+    error = function(e){
+      warning(glue::glue("Prices for {symbol} were not found."))
+    })
+    
     closeAllConnections()
-    prices$name <- symbol
     return(prices)
   })
   
