42599506a3362de9abebcc9402bcef2d4d239b76
aljrico <aljrico@gmail.com>
2021-09-11 16:16:17 +0200

    explicit call from testthat

--- tests/testthat/test-log_in.R
+++ tests/testthat/test-log_in.R
@@ -1,9 +1,9 @@
-test_that("log_in creates a connection instance", {
+testthat::test_that("log_in creates a connection instance", {
   rfinance::log_in(NULL, NULL)
   testthat::expect_true(exists("rfinanceConnection"))
 })
 
-test_that("log_out removes a connection instance", {
+testthat::test_that("log_out removes a connection instance", {
   rfinance::log_in(NULL, NULL)
   rfinance::log_out()
   testthat::expect_false(exists("rfinanceConnection"))
